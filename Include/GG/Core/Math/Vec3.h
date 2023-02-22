3eeb15a45b59cd3dba461c54632255fac782871c
OstGeneralen <kasper.esbjornsson@gmail.com>
2023-02-22 00:26:14 +0000

    Add initial Vec3 wrapper for GLM

---
+++ Include/GG/Core/Math/Vec3.h
@@ -0,0 +1,26 @@
+#include <glm/Vec3.hpp>
+
+namespace gg
+{
+	using Vec3_Back = glm::vec3;
+
+	class CVec3
+	{
+	public:
+		CVec3();
+		CVec3( float x, float y, float z );
+		CVec3( const CVec3& o );
+
+
+		explicit operator Vec3_Back();
+
+	private:
+		Vec3_Back _vec3;
+        
+    public:
+    	float& X;
+		float& Y;
+		float& Z;
+
+	};
+} // namespace gg
---
+++ Source/GG/Core/Math/Vec3.cpp
@@ -0,0 +1,30 @@
+#include "GG/Core/Math/Vec3.h"
+
+gg::CVec3::CVec3()
+	: _vec3( 0, 0, 0 )
+	, X( _vec3.x )
+	, Y( _vec3.y )
+	, Z( _vec3.z )
+{
+}
+
+gg::CVec3::CVec3( float x, float y, float z )
+	: _vec3( x, y, z )
+	, X( _vec3.x )
+	, Y( _vec3.y )
+	, Z( _vec3.z )
+{
+}
+
+gg::CVec3::CVec3(const CVec3& o)
+    : _vec3(o._vec3)
+    , X( _vec3.x )
+	, Y( _vec3.y )
+	, Z( _vec3.z )
+{
+}
+
+gg::CVec3::operator gg::Vec3_Back()
+{
+    return _vec3;
+}
