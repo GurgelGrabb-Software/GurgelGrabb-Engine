4a5fcb43d2d584a672093005968bf24d061a95a5
OstGeneralen <kasper.esbjornsson@gmail.com>
2023-02-25 16:28:08 +0000

    Fix image not building due to wchar* char* mismatch

--- Source/GG/Rendering/Image.cpp
+++ Source/GG/Rendering/Image.cpp
@@ -10,7 +10,8 @@ using namespace gg;
 std::shared_ptr<CImage> CImage::LoadFromFile(const std::filesystem::path& filepath)
 {
     int w, h, ch;
-    const PixelData data = (PixelData)stbi_load(filepath.c_str(), &w, &h, &ch, 0);
+    
+    const PixelData data = (PixelData)stbi_load(filepath.string().c_str(), &w, &h, &ch, 0);
 
     auto image = std::make_shared<CImage>(w, h, static_cast<ETextureFormat>(ch-1), data);
 
@@ -20,7 +21,7 @@ std::shared_ptr<CImage> CImage::LoadFromFile(const std::filesystem::path& filepa
 void CImage::SaveToFile(CImage& image, const std::filesystem::path& filepath)
 {
     int channels = static_cast<int>(image._format) + 1;
-    stbi_write_png(filepath.c_str(), image._width, image._height, channels, image._pixels, image._width * channels);
+    stbi_write_png(filepath.string().c_str(), image._width, image._height, channels, image._pixels, image._width * channels);
 }
 
 CImage::CImage(unsigned width, unsigned height, ETextureFormat format, const PixelData data) : 
