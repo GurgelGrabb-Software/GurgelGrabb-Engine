4ccf794a6f4fd339b83ec81dfae9fafd37179c33
OstGeneralen <kasper.esbjornsson@gmail.com>
2023-02-25 16:19:26 +0000

    Clean up List.h

--- Include/GG/Common/Container/List.h
+++ Include/GG/Common/Container/List.h
@@ -3,39 +3,145 @@
 
 #pragma once
 #include "GG/Common/Helpers/MemUtil.h"
+
 #include <initializer_list>
 #include <type_traits>
 #include <utility>
 
 namespace gg
 {
-	// DECL
 	template < typename T, typename TSize = size_t >
 	class TList
 	{
-	public:
-		// CTOR/DTOR
-		TList();
-		TList( TSize reserve );
-		TList( const TList< T, TSize >& other );
-		TList( TList< T, TSize >&& other ) noexcept;
-		TList( std::initializer_list< T > initList );
-		~TList();
+	public: // CTOR/DTOR
+		TList()
+			: _elems( new T[16] )
+			, _reserved( 16 )
+			, _size( 0 )
+		{
+		}
 
-		// OPERATOR
-		[[nodiscard]] T& operator[]( TSize i );
-		[[nodiscard]] const T& operator[]( TSize i ) const;
+		TList( TSize reserve )
+			: _elems( nullptr )
+			, _reserved( 0 )
+			, _size( 0 )
+		{
+			Reserve( reserve );
+		}
 
-		// MUTATORS
-		T& Add( const T& cpy );
-		void Remove( TSize i );
-		void Reserve( TSize s );
+		TList( const TList< T, TSize >& other )
+			: _elems( new T[other._reserved] )
+			, _reserved( other._reserved )
+			, _size( other._size )
+		{
+			BufferCopy< T >( other._elems, _elems, _size );
+		}
 
-		// GETTERS
-		TSize Size() const;
-		TSize Reserved() const;
+		TList( TList< T, TSize >&& other ) noexcept
+			: _elems( std::exchange( other._elems, nullptr ) )
+			, _reserved( std::exchange( other._reserved, 0 ) )
+			, _size( std::exchange( other._size, 0 ) )
+		{
+		}
 
-	public:
+		TList( std::initializer_list< T > initList )
+			: _elems( new T[initList.size()] )
+			, _reserved( initList.size() )
+			, _size( initList.size() )
+		{
+			TSize i = 0;
+			for ( const auto& ilt : initList )
+			{
+				_elems[i] = ilt;
+				i++;
+			}
+		}
+
+		~TList()
+		{
+			if ( _elems )
+			{
+				delete[] _elems;
+			}
+		}
+
+	public: // OPERATORS
+		[[nodiscard]] T& operator[]( TSize i )
+		{
+			return _elems[i];
+		}
+
+		[[nodiscard]] const T& operator[]( TSize i ) const
+		{
+			return _elems[i];
+		}
+
+	public: // MUTATORS
+
+		template<typename ... TArgs>
+		T& Emplace( TArgs&& ... args )
+		{
+			
+		}
+
+		T& Add( const T& cpy )
+		{
+			if ( _size == _reserved )
+			{
+				Reserve( _reserved * 2 );
+			}
+
+			_elems[_size] = cpy;
+			++_size;
+			return _elems[_size - 1];
+		}
+
+		void PopBack( T* set = nullptr )
+		{
+			if ( set != nullptr )
+			{
+				*set = _elems[_size - 1];
+			}
+
+			Remove( _size - 1 );
+		}
+
+		void Remove( TSize i )
+		{
+			_elems[i] = _elems[_size - 1];
+			_size--;
+		}
+
+		void Reserve( TSize s )
+		{
+			T* newBuf = new T[s];
+			if ( _elems )
+			{
+				BufferCopy< T >( _elems, newBuf, (unsigned int)_size );
+				delete[] _elems;
+			}
+
+			_reserved = s;
+			_elems = newBuf;
+		}
+
+	public: // ACCESSORS
+		bool IsEmpty() const
+		{
+			return _size == 0u;
+		}
+
+		TSize Size() const
+		{
+			return _size;
+		}
+
+		TSize Reserved() const
+		{
+			return _reserved;
+		}
+
+	public: // ITERATORS
 		class TIt
 		{
 		public:
@@ -126,115 +232,4 @@ namespace gg
 		TSize _reserved;
 		TSize _size;
 	};
-
-// clang-format off
-#define LIST_T template < typename T, typename TSize >
-#define LIST TList<T, TSize>
-	// clang-format on
-
-	// DEF
-	LIST_T LIST::TList()
-		: _elems( new T[16] )
-		, _reserved( 16 )
-		, _size( 0 )
-	{
-	}
-
-	LIST_T LIST::TList( TSize reserve )
-		: _elems( nullptr )
-		, _reserved( 0 )
-		, _size( 0 )
-	{
-		Reserve( reserve );
-	}
-
-	LIST_T LIST::TList( const TList< T, TSize >& other )
-		: _elems( new T[other._reserved] )
-		, _reserved( other._reserved )
-		, _size( other._size )
-	{
-		BufferCopy< T >( other._elems, _elems, _size );
-	}
-
-	LIST_T LIST::TList( TList< T, TSize >&& other ) noexcept
-		: _elems( std::exchange( other._elems, nullptr ) )
-		, _reserved( std::exchange( other._reserved, 0 ) )
-		, _size( std::exchange( other._size, 0 ) )
-	{
-	}
-
-	LIST_T LIST::TList( std::initializer_list< T > initList )
-		: _elems( new T[initList.size()] )
-		, _reserved( initList.size() )
-		, _size( initList.size() )
-	{
-		TSize i = 0;
-		for ( const auto& ilt : initList )
-		{
-			_elems[i] = ilt;
-			i++;
-		}
-	}
-
-	LIST_T LIST::~TList()
-	{
-		if ( _elems )
-		{
-			delete[] _elems;
-		}
-	}
-
-	LIST_T T& LIST::operator[]( TSize i )
-	{
-		return _elems[i];
-	}
-
-	LIST_T const T& LIST::operator[]( TSize i ) const
-	{
-		return _elems[i];
-	}
-
-	LIST_T T& LIST::Add( const T& cpy )
-	{
-		if ( _size == _reserved )
-		{
-			Reserve( _reserved * 2 );
-		}
-
-		_elems[_size] = cpy;
-		++_size;
-		return _elems[_size - 1];
-	}
-
-	LIST_T void LIST::Remove( TSize i )
-	{
-		_elems[i] = _elems[_size - 1];
-		_size--;
-	}
-
-	LIST_T void LIST::Reserve( TSize s )
-	{
-		T* newBuf = new T[s];
-		if(_elems)
-		{
-			BufferCopy< T >( _elems, newBuf, (unsigned int)_size );
-			delete[] _elems;
-		}
-
-		_reserved = s;
-		_elems = newBuf;
-	}
-
-	LIST_T TSize LIST::Size() const
-	{
-		return _size;
-	}
-
-	LIST_T TSize LIST::Reserved() const
-	{
-		return _reserved;
-	}
-#undef LIST_T
-#undef LIST
-
 } // namespace gg
