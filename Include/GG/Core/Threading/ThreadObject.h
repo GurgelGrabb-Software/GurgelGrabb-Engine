4f5a3176d59eb1e138f1326ebd54d9148692eda8
Joakim Bensryd <joakimbensryd@gmail.com>
2023-03-05 12:38:25 +0100

    Fixed bug in threadpool where a thread wouldn't reset it's work function when complete

--- Include/GG/Core/Threading/ThreadObject.h
+++ Include/GG/Core/Threading/ThreadObject.h
@@ -7,36 +7,37 @@
 
 namespace gg
 {
-    namespace threading_internal
-    {
-        class IWorkFunction;
+	namespace threading_internal
+	{
+		class IWorkFunction;
 
-        class CThreadObject
-        {
-        public:
-            CThreadObject& operator=( CThreadObject&& ) = delete;
-            CThreadObject( CThreadObject&& ) = delete;
+		class CThreadObject
+		{
+		public:
+			CThreadObject& operator=( CThreadObject&& ) = delete;
+			CThreadObject( CThreadObject&& ) = delete;
 
-            CThreadObject( const CThreadObject& ) = delete;
-            CThreadObject& operator=( const CThreadObject& ) = delete;
+			CThreadObject( const CThreadObject& ) = delete;
+			CThreadObject& operator=( const CThreadObject& ) = delete;
 
-            CThreadObject();
-            ~CThreadObject();
+			CThreadObject();
+			~CThreadObject();
 
-            IWorkFunction* GetWorkFunction() const;
+			IWorkFunction* GetWorkFunction() const;
+			void Reset();
 
-            void StartWork( std::shared_ptr< IWorkFunction > inWorkFunction );
+			void StartWork( std::shared_ptr< IWorkFunction > inWorkFunction );
 
-        private:
-            void Loop();
+		private:
+			void Loop();
 
-            std::atomic_bool terminateThread;
+			std::atomic_bool terminateThread;
 
-            std::mutex workMutex;
-            std::shared_ptr< IWorkFunction > workFunction;
+			std::mutex workMutex;
+			std::shared_ptr< IWorkFunction > workFunction;
 
-            std::condition_variable condition;
-            std::thread thread;
-        };
-    }
-}
+			std::condition_variable condition;
+			std::thread thread;
+		};
+	} // namespace threading_internal
+} // namespace gg
--- Source/GG/Core/Threading/ThreadObject.cpp
+++ Source/GG/Core/Threading/ThreadObject.cpp
@@ -5,49 +5,54 @@
 using namespace gg;
 
 threading_internal::CThreadObject::CThreadObject()
-    : terminateThread( false )
+	: terminateThread( false )
 {
-    thread = std::thread( std::bind( &CThreadObject::Loop, this ) );
+	thread = std::thread( std::bind( &CThreadObject::Loop, this ) );
 }
 
 threading_internal::CThreadObject::~CThreadObject()
 {
-    std::unique_lock< std::mutex > lock( workMutex );
-    terminateThread = true;
+	std::unique_lock< std::mutex > lock( workMutex );
+	terminateThread = true;
 
-    condition.notify_one();
+	condition.notify_one();
 
-    lock.unlock();
+	lock.unlock();
 
-    thread.join();
+	thread.join();
 }
 
 threading_internal::IWorkFunction* threading_internal::CThreadObject::GetWorkFunction() const
 {
-    return workFunction.get();
+	return workFunction.get();
+}
+
+void threading_internal::CThreadObject::Reset()
+{
+	workFunction = nullptr;
 }
 
 void threading_internal::CThreadObject::StartWork( std::shared_ptr< IWorkFunction > inWorkFunction )
 {
-    std::unique_lock< std::mutex > lock( workMutex );
-    workFunction = inWorkFunction;
+	std::unique_lock< std::mutex > lock( workMutex );
+	workFunction = inWorkFunction;
 
-    condition.notify_one();
+	condition.notify_one();
 }
 
 void threading_internal::CThreadObject::Loop()
 {
-    while ( !terminateThread )
-    {
-        std::unique_lock< std::mutex > lock( workMutex );
-        condition.wait( lock, [this]() { return ( workFunction != nullptr && !workFunction->IsCompleted() ) || terminateThread; } );
+	while ( !terminateThread )
+	{
+		std::unique_lock< std::mutex > lock( workMutex );
+		condition.wait( lock, [this]() { return ( workFunction != nullptr && !workFunction->IsCompleted() ) || terminateThread; } );
 
-        if ( !terminateThread )
-        {
-            if ( workFunction && !workFunction->IsCompleted() )
-            {
-                workFunction->Work();
-            }
-        }
-    }
+		if ( !terminateThread )
+		{
+			if ( workFunction && !workFunction->IsCompleted() )
+			{
+				workFunction->Work();
+			}
+		}
+	}
 }
