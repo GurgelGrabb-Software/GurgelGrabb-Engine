e08091945f82ca9f13b9a436770858895b3f332f
Joakim Bensryd <joakimbensryd@gmail.com>
2023-03-14 17:32:02 +0100

    Turned off clang format in gl setup.

--- Include/GG/Rendering/GLInclude.h
+++ Include/GG/Rendering/GLInclude.h
@@ -1,4 +1,8 @@
 #pragma once
 
+// clang-format off
+
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
+
+// clang-format on
--- Source/GG/Rendering/GLSetup.cpp
+++ Source/GG/Rendering/GLSetup.cpp
@@ -1,3 +1,7 @@
+// clang-format off
+
 #define GLAD_GL_IMPLEMENTATION
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
+
+// clang-format on
