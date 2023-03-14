94a2348ef96ffa4d6c91523fca707d55c0a729db
Joakim Bensryd <joakimbensryd@gmail.com>
2023-03-14 17:40:02 +0100

    Clang formatted

--- Source/GG/Rendering/ShaderProgram.cpp
+++ Source/GG/Rendering/ShaderProgram.cpp
@@ -1,75 +1,75 @@
 #include "GG/Rendering/ShaderProgram.h"
 
+#include "GG/Rendering/GLInclude.h"
 #include "GG/Rendering/Shader.h"
 #include "GG/Rendering/Texture.h"
-
-#include "GG/Rendering/GLInclude.h"
 
 #include <iostream>
 
 using namespace gg;
 
-CShaderProgram::CShaderProgram(const CShader& vertexShader, const CShader& pixelShader)
+CShaderProgram::CShaderProgram( const CShader& vertexShader, const CShader& pixelShader )
 {
-    _handle = glCreateProgram();
-    glAttachShader(_handle, vertexShader.GetHandle());
-    glAttachShader(_handle, pixelShader.GetHandle());
-    glLinkProgram(_handle);
+	_handle = glCreateProgram();
+	glAttachShader( _handle, vertexShader.GetHandle() );
+	glAttachShader( _handle, pixelShader.GetHandle() );
+	glLinkProgram( _handle );
 
-    int result;
-    glGetProgramiv(_handle, GL_LINK_STATUS, &result);
-    if(!result) {
-        char log[512];
-        glGetProgramInfoLog(_handle, 512, NULL, log);
-        std::cout << "ShaderProgram: " << log << std::endl;
-    }
+	int result;
+	glGetProgramiv( _handle, GL_LINK_STATUS, &result );
+	if ( !result )
+	{
+		char log[512];
+		glGetProgramInfoLog( _handle, 512, NULL, log );
+		std::cout << "ShaderProgram: " << log << std::endl;
+	}
 }
 
 CShaderProgram::~CShaderProgram()
 {
-    glDeleteProgram(_handle);
+	glDeleteProgram( _handle );
 }
 
 void CShaderProgram::Bind() const
 {
-    glUseProgram(_handle);
+	glUseProgram( _handle );
 }
 
-void CShaderProgram::SetUniform(const char* uniform, const CTexture& v, unsigned unit)
+void CShaderProgram::SetUniform( const char* uniform, const CTexture& v, unsigned unit )
 {
-    glActiveTexture(GL_TEXTURE0 + unit);
-    v.Bind();
+	glActiveTexture( GL_TEXTURE0 + unit );
+	v.Bind();
 
-    int location = glGetUniformLocation(_handle, uniform);
-    glUniform1i(location, unit);
+	int location = glGetUniformLocation( _handle, uniform );
+	glUniform1i( location, unit );
 }
 
-void CShaderProgram::SetUniform(const char* uniform, const gg::CMat4x4& v)
+void CShaderProgram::SetUniform( const char* uniform, const gg::CMat4x4& v )
 {
-    int location = glGetUniformLocation(_handle, uniform);
-    glUniformMatrix4fv(location, 1, false, v.GetData());
+	int location = glGetUniformLocation( _handle, uniform );
+	glUniformMatrix4fv( location, 1, false, v.GetData() );
 }
 
-void CShaderProgram::SetUniform(const char* uniform, const gg::SFloat4& v)
+void CShaderProgram::SetUniform( const char* uniform, const gg::SFloat4& v )
 {
-    int location = glGetUniformLocation(_handle, uniform);
-    glUniform4fv(location, 1, &v.x);
+	int location = glGetUniformLocation( _handle, uniform );
+	glUniform4fv( location, 1, &v.x );
 }
 
-void CShaderProgram::SetUniform(const char* uniform, const gg::SFloat3& v)
+void CShaderProgram::SetUniform( const char* uniform, const gg::SFloat3& v )
 {
-    int location = glGetUniformLocation(_handle, uniform);
-    glUniform3fv(location, 1, &v.x);
+	int location = glGetUniformLocation( _handle, uniform );
+	glUniform3fv( location, 1, &v.x );
 }
 
-void CShaderProgram::SetUniform(const char* uniform, const gg::SFloat2& v)
+void CShaderProgram::SetUniform( const char* uniform, const gg::SFloat2& v )
 {
-    int location = glGetUniformLocation(_handle, uniform);
-    glUniform2fv(location, 1, &v.x);
+	int location = glGetUniformLocation( _handle, uniform );
+	glUniform2fv( location, 1, &v.x );
 }
 
-void CShaderProgram::SetUniform(const char* uniform, float v)
+void CShaderProgram::SetUniform( const char* uniform, float v )
 {
-    int location = glGetUniformLocation(_handle, uniform);
-    glUniform1fv(location, 1, &v);
+	int location = glGetUniformLocation( _handle, uniform );
+	glUniform1fv( location, 1, &v );
 }
--- Source/GG/Rendering/VertexBuffer.cpp
+++ Source/GG/Rendering/VertexBuffer.cpp
@@ -1,110 +1,109 @@
 #include "GG/Rendering/VertexBuffer.h"
 
+#include "GG/Rendering/GLInclude.h"
 #include "GG/Rendering/RenderTypes.h"
 
-#include "GG/Rendering/GLInclude.h"
-
-#include <cmath>
 #include <cassert>
+#include <cmath>
 
 using namespace gg;
 
-CVertexBuffer::CVertexBuffer(EVertexFormat format, EVertexAccessMode accessMode) : 
-_format(format),
-_accessMode(accessMode)
+CVertexBuffer::CVertexBuffer( EVertexFormat format, EVertexAccessMode accessMode )
+	: _format( format )
+	, _accessMode( accessMode )
 {
-    glGenVertexArrays(1, &_handle);
-    glBindVertexArray(_handle);
+	glGenVertexArrays( 1, &_handle );
+	glBindVertexArray( _handle );
 
-    if (HasFlag(format, EVertexAttributeType::Position))
-    {
-        addAttribute(EVertexAttributeType::Position, {});
-    }
+	if ( HasFlag( format, EVertexAttributeType::Position ) )
+	{
+		addAttribute( EVertexAttributeType::Position, {} );
+	}
 
-    if (HasFlag(format, EVertexAttributeType::Color))
-    {
-        addAttribute(EVertexAttributeType::Color, {});
-    }
+	if ( HasFlag( format, EVertexAttributeType::Color ) )
+	{
+		addAttribute( EVertexAttributeType::Color, {} );
+	}
 
-    if (HasFlag(format, EVertexAttributeType::UV))
-    {
-        addAttribute(EVertexAttributeType::UV, {});
-    }
+	if ( HasFlag( format, EVertexAttributeType::UV ) )
+	{
+		addAttribute( EVertexAttributeType::UV, {} );
+	}
 }
 
 CVertexBuffer::~CVertexBuffer()
 {
-    glDeleteBuffers(1, &_handle);
+	glDeleteBuffers( 1, &_handle );
 }
 
 void CVertexBuffer::Bind() const
 {
-    glBindVertexArray(_handle);
+	glBindVertexArray( _handle );
 }
 
-const std::vector<float>& CVertexBuffer::GetAttributeData(EVertexAttributeType type) const
+const std::vector< float >& CVertexBuffer::GetAttributeData( EVertexAttributeType type ) const
 {
-    auto it = _attributeHandles.find(type);
-    assert(it != _attributeHandles.end());
+	auto it = _attributeHandles.find( type );
+	assert( it != _attributeHandles.end() );
 
-    return it->second.data;
+	return it->second.data;
 }
 
-void CVertexBuffer::SetAttributeData(EVertexAttributeType type, const std::vector<float>& data)
+void CVertexBuffer::SetAttributeData( EVertexAttributeType type, const std::vector< float >& data )
 {
-    auto it = _attributeHandles.find(type);
-    if (it == _attributeHandles.end())
-    {
-        assert(false && "Attribute does not exist.");
-        return;
-    }
+	auto it = _attributeHandles.find( type );
+	if ( it == _attributeHandles.end() )
+	{
+		assert( false && "Attribute does not exist." );
+		return;
+	}
 
-    it->second.data = data;
+	it->second.data = data;
 
-    _vertexCount = data.size() / GetNumOfElements(type);
+	_vertexCount = data.size() / GetNumOfElements( type );
 
-    Bind();
+	Bind();
 
-    glBindBuffer(GL_ARRAY_BUFFER, it->second.handle);
-    glBufferData(GL_ARRAY_BUFFER, data.size() * sizeof(float), data.data(), ConvertToGLType(_accessMode));
+	glBindBuffer( GL_ARRAY_BUFFER, it->second.handle );
+	glBufferData( GL_ARRAY_BUFFER, data.size() * sizeof( float ), data.data(), ConvertToGLType( _accessMode ) );
 
-    glEnableVertexAttribArray(it->second.index);
-    glVertexAttribPointer(it->second.index, GetNumOfElements(type), GL_FLOAT, false, GetByteSize(type), (void*)0);
+	glEnableVertexAttribArray( it->second.index );
+	glVertexAttribPointer( it->second.index, GetNumOfElements( type ), GL_FLOAT, false, GetByteSize( type ), (void*)0 );
 }
 
-void CVertexBuffer::addAttribute(EVertexAttributeType type, const std::vector<float>& data)
+void CVertexBuffer::addAttribute( EVertexAttributeType type, const std::vector< float >& data )
 {
-    auto index = _attributeHandles.size();
+	auto index = _attributeHandles.size();
 
-    auto& attrHandle = _attributeHandles[type];
-    attrHandle.index = index;
-    glGenBuffers(1, &attrHandle.handle);
+	auto& attrHandle = _attributeHandles[type];
+	attrHandle.index = index;
+	glGenBuffers( 1, &attrHandle.handle );
 
-    if (!data.empty())
-    {
-        SetAttributeData(type, data);
-    }
+	if ( !data.empty() )
+	{
+		SetAttributeData( type, data );
+	}
 }
 
-bool CVertexBuffer::hasAttribute(EVertexAttributeType type)
+bool CVertexBuffer::hasAttribute( EVertexAttributeType type )
 {
-    return _attributeHandles.find(type) != _attributeHandles.end();
+	return _attributeHandles.find( type ) != _attributeHandles.end();
 }
 
 bool CVertexBuffer::ValidateAttributes() const
 {
-    for (auto& attrHandle : _attributeHandles)
-    {
-        if (attrHandle.second.data.size() / GetNumOfElements(attrHandle.first) == _vertexCount)
-        {
-            assert(false && "Attributes are not synced!");
-            return false;
-        }
-    }
-    return true;
+	for ( auto& attrHandle : _attributeHandles )
+	{
+		if ( attrHandle.second.data.size() / GetNumOfElements( attrHandle.first ) == _vertexCount )
+		{
+			assert( false && "Attributes are not synced!" );
+			return false;
+		}
+	}
+	return true;
 }
 
-void CVertexBuffer::SetPrimitiveType(EPrimitiveType primitiveType)
+void CVertexBuffer::SetPrimitiveType( EPrimitiveType primitiveType )
 {
-    _primitiveType = primitiveType;
+	_primitiveType = primitiveType;
 }
